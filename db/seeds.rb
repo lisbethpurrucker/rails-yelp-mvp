23a0d19c7f695f2124e37d4cfe27cf738687c169
Lisbeth Purrucker <lisbeth@cuba-buddy.de>
2021-02-17 14:17:11 +0100

    controller, seeds & routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east =  { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+plantbase =  { name: "Plantbase", address: "56B Shoreditch High St, London E1 6PQ", category: "japanese" }
+carné =  { name: "Carné", address: "59A Shoreditch High St, London E1 6PQ", category: "belgian" }
+ca_va =  { name: "Ca va?", address: "16C Shoreditch High St, London E1 6PQ", category: "french" }
+
+[ dishoom, pizza_east, plantbase, carné,ca_va ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
